72048d96c3174092c61434b31e8cae63a06945eb
岩崎仁是 <yanqirenshi@gmail.com>
2020-07-12 10:46:47 +0900

    add .env and build

---
+++ docs/precache-manifest.3c68d3defaeb4a3b4ec5e92cd3a40c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f019829d4301122c7995bb57a082ccc4",
+    "url": "/Asshole/index.html"
+  },
+  {
+    "revision": "aaae46fe812f320a211d",
+    "url": "/Asshole/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4cd7af7dd9209e90b2d9",
+    "url": "/Asshole/static/js/2.b4b10098.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Asshole/static/js/2.b4b10098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaae46fe812f320a211d",
+    "url": "/Asshole/static/js/main.f48b92e1.chunk.js"
+  },
+  {
+    "revision": "21a93c65f9f78a795025",
+    "url": "/Asshole/static/js/runtime-main.63e8c60e.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c38373388031a880328019250fdee4cc.js"
+  "/Asshole/precache-manifest.3c68d3defaeb4a3b4ec5e92cd3a40c11.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Asshole/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
